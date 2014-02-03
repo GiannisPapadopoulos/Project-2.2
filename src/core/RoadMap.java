29de0a68bd15e8aab53153f96227b084f93f0f48
Giannis_Papadopoulos <shaihulud77@gmail.com>
2014-02-03 18:10:22 +0100

    Initial Commit

---
+++ src/core/RoadMap.java
@@ -0,0 +1,34 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class RoadMap {
+	private ArrayList<Road> roads = new ArrayList<Road>();
+	private HashSet<RoadEndpoint> endpoints = new HashSet<RoadEndpoint>();
+	
+	public RoadMap(Road[] initialRoads){
+		for (Road r: initialRoads){
+			addRoad(r);
+		}
+	}
+	public RoadMap(){
+		this(new Road[0]);
+	}
+	public void addRoad(Road road){
+		
+		//The fact that these are HashSets makes sure that roads aren't added more than once.
+		endpoints.add(road.getStart());
+		endpoints.add(road.getEnd());
+		
+		roads.add(road);
+	}
+	
+	public Iterable<RoadEndpoint> getEndpoints(){
+		return (Iterable<RoadEndpoint>)endpoints.clone(); //I'm cloning because we don't want outsiders modifying our roads list
+	}
+	
+	public Iterable<Road> getRoads(){
+		return (Iterable<Road>) roads.clone(); 
+	}
+}
---
+++ src/io/RoadMapWriter.java
@@ -0,0 +1,10 @@
+package io;
+
+import java.io.File;
+
+
+public class RoadMapWriter {
+	public static void writeRoadMap(File file, core.RoadMap map){
+		
+	}
+}

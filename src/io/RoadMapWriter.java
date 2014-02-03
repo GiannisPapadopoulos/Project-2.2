6905e386ea69e25350eb1d2dcd5936c00fcbbfd3
Frederik Creemers <frederikcreemers@gmail.com>
2014-02-03 19:11:14 +0100

    RoadMapWriter

--- src/io/RoadMapWriter.java
+++ src/io/RoadMapWriter.java
@@ -1,10 +1,20 @@
 package io;
 
-import java.io.File;
-
+import java.io.PrintWriter;
+import java.io.IOException;
 
 public class RoadMapWriter {
-	public static void writeRoadMap(File file, core.RoadMap map){
+	public static void writeRoadMap(String filename, core.RoadMap map) throws IOException{
+		PrintWriter pw = new PrintWriter(filename);
 		
+		for (core.RoadEndpoint ep: map.getEndpoints()){
+			String line = ep.getLabel() + " " + ep.getX() + " " + ep.getY();
+			pw.println(line);
+		}
+		
+		pw.println();
+		for (core.Road road: map.getRoads()){
+			String line = road.getStart().getLabel() + " " + road.getEnd().getLabel() + " " + road.getLength() + " " + road.getSpeed();
+		}
 	}
 }

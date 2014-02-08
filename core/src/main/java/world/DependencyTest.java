d224d2fa849a2d457ea10a08729213fbd588a5d6
Peter Jendr <jendrichovsky@seznam.cz>
2014-02-09 00:24:38 +0100

    Working Maven Project With Lombok. Please, create your branches from this version of develop. (Redo previously created branches...)

---
+++ core/src/main/java/world/DependencyTest.java
@@ -0,0 +1,11 @@
+package world;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class DependencyTest {
+
+	private boolean test;
+}
---
+++ desktop/src/main/java/desktop/Main.java
@@ -0,0 +1,11 @@
+package desktop;
+
+import world.DependencyTest;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		DependencyTest dt = new DependencyTest(true);
+	}
+}

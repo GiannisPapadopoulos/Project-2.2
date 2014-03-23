06652e00c7b05f509debe260b09e5ddc88405798
ramyalsharif <ramyalsharif@gmail.com>
2014-03-23 16:37:53 +0100

    buttons linked

--- core/src/main/java/trafficsim/screens/SuperScreen.java
+++ core/src/main/java/trafficsim/screens/SuperScreen.java
@@ -12,7 +12,7 @@ import lombok.val;
 import trafficsim.systems.InputEditorSystem;
 import trafficsim.systems.InputSystem;
 import ui.tables.SidePanels;
-import ui.tables.SpeedSettings;
+import ui.tables.TransitionButtons;
 import utils.Assets;
 
 import com.badlogic.gdx.Gdx;
@@ -20,11 +20,14 @@ import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener.ChangeEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import editor.MousePosition;
 
@@ -109,9 +112,9 @@ public abstract class SuperScreen implements Screen {
 
 	private void populateCommonLayers() {
 		
-		Table sidePanels = new SidePanels();
+		SidePanels sidePanels = new SidePanels();
 		Table t = new Table();
-		Table settings = new SpeedSettings();
+		TransitionButtons settings = new TransitionButtons();
 		ArrayList<TextButton> buttons = new ArrayList<TextButton>();
 
 		TextButton simButton = new TextButton("Simulation", Assets.skin);
@@ -134,23 +137,23 @@ public abstract class SuperScreen implements Screen {
 
 		t.setFillParent(true);
 		
-		
-		
 		getUILayer().addActor(sidePanels);
 		getUILayer().addActor(settings);
-
-		for (val button : buttons)
-			button.addListener(new ChangeListener() {
-				@Override
-				public void changed(ChangeEvent event, Actor actor) {
-					if (actor.getName() == "Simulation")
-						switchToScreen(screens.getSimulationScreen());
-					else if (actor.getName() == "Editor")
-						switchToScreen(screens.getEditorScreen());
-					else if (actor.getName() == "Statistics")
-						switchToScreen(screens.getStatisticsScreen());
-				}
-			});
+	
+			
+			for (val button : sidePanels.getTransitionButtons().getButtons())
+				button.addListener(new ChangeListener() {
+					@Override
+					public void changed(ChangeEvent event, Actor actor) {
+						if (actor.getName() == "Simulation")
+							switchToScreen(screens.getSimulationScreen());
+						else if (actor.getName() == "Editor")
+							switchToScreen(screens.getEditorScreen());
+						else if (actor.getName() == "Statistics")
+							switchToScreen(screens.getStatisticsScreen());
+					}
+				});
+		
 
 		populateUILayer();
 		populateWorldLayer();
